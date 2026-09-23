6bc188d1fd592d032f804e4d79edda9280c94774
ShawanMatiasss <shawan.magalhaes@aluno.ce.gov.br>
2026-09-23 13:27:52 -0300

    Add sleep hours check with corresponding messages

---
+++ index.js
@@ -0,0 +1,10 @@
+// Define a quantidade de horas dormidas
+let horasDormidas = 5;
+// Verifica a quantidade de horas dormidas e exibe uma mensagem correspondente
+if (horasDormidas >= 8) {
+  console.log("Você dormiu bem!");
+} else if (horasDormidas >= 6) {
+  console.log("Sono razoável, mas poderia ser melhor.");
+} else {
+  console.log("Você dormiu pouco, cuidado com o cansaço!");
+}
